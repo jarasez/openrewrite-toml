035fe50f3d122d4a86234ecd1b07946ff8328c27
HETEA Maria-Mihaela <maria-mihaela.hetea@ext.ec.europa.eu>
2025-01-13 07:20:16 +0200

    [OpenRewrite] Toml bug with bundles

---
+++ src/main/java/test/openrewrite/openrewritetoml/OpenrewriteTomlApplication.java
@@ -0,0 +1,13 @@
+package test.openrewrite.openrewritetoml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenrewriteTomlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenrewriteTomlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/openrewrite/openrewritetoml/OpenrewriteTomlApplicationTests.java
@@ -0,0 +1,13 @@
+package test.openrewrite.openrewritetoml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenrewriteTomlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
